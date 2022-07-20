2dfd38a7af4514403dd707f4961d8451ce37c490
jhsong04 <78022008+jhsong04@users.noreply.github.com>
2022-07-20 14:35:42 +0900

    update
    week3

--- week3.py
+++ week3.py
@@ -3,28 +3,30 @@ import torch
 
 def train_softmax_regression():
     x_data = torch.Tensor(
-            [[10, 30, 20],
-             [30, 10, 30],
-             [40, 70, 70],
-             [80, 0, 0],
-             [90, 80, 50],
-             [80, 100, 100]])  # [6, 3]
+        [[1, 2, 1, 1],
+         [2, 1, 3, 2],
+         [3, 1, 3, 4],
+         [4, 1, 5, 5],
+         [1, 5, 5, 5],
+         [1, 2, 5, 6],
+         [1, 6, 6, 6],
+         [1, 7, 7, 7]])  # [6, 3]
 
     # x_data = x_data.transpose(1, 0)
 
     # [6, 3] * [1, 3, 3]
 
     y_data = torch.tensor(
-        [0, 0, 1, 1, 2, 2]
+        [2, 2, 2, 1, 1, 1, 0, 0]
     )
 
-    W = torch.ones((3, 3), requires_grad=True)
+    W = torch.ones((3, 4), requires_grad=True)
     b = torch.zeros(3, requires_grad=True)
 
     loss_fn = torch.nn.CrossEntropyLoss()
 
 
-    optimizer = torch.optim.SGD([W, b], lr=0.01)
+    optimizer = torch.optim.SGD([W, b], lr=0.1)
 
     nb_epochs = 30000
     for epoch in range(nb_epochs + 1):
